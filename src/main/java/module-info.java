336a484560cf5c5cf77abbcf33d0714b6aeb1cf2
Fernando W S Furtado <feh.wilinando@gmail.com>
2018-02-14 00:59:18 -0200

    Setup do módulo web

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module br.com.caelum.mog.application {
+
+    requires spring.boot.autoconfigure;
+    requires spring.boot;
+    requires spring.beans;
+    requires spring.web;
+
+    requires br.com.caelum.mog.infra;
+}
