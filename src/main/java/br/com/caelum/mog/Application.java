63be93a09b1d0cd2a693c2658c0f4ac1a57eacf9
Fernando W S Furtado <feh.wilinando@gmail.com>
2018-02-18 23:11:01 -0300

    Armazenando o contexto da aplicação

--- src/main/java/br/com/caelum/mog/Application.java
+++ src/main/java/br/com/caelum/mog/Application.java
@@ -2,12 +2,16 @@ package br.com.caelum.mog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
+    private static ApplicationContext context;
+
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        context = SpringApplication.run(Application.class, args);
     }
 
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ module br.com.caelum.mog.application {
     requires spring.boot;
     requires spring.beans;
     requires spring.web;
+    requires spring.context;
 
     requires br.com.caelum.mog.infra;
 }
