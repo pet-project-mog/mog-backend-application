1b20661bd5cd821b67c9d05a878335049dbc27ad
Fernando W S Furtado <feh.wilinando@gmail.com>
2018-02-20 03:52:01 -0300

    [Technical Debt] Removendo atributo não utilizado

--- src/main/java/br/com/caelum/mog/Application.java
+++ src/main/java/br/com/caelum/mog/Application.java
@@ -2,16 +2,13 @@ package br.com.caelum.mog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
-    private static ApplicationContext context;
 
     public static void main(String[] args) {
-        context = SpringApplication.run(Application.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
