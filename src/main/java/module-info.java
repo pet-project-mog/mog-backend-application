d70065c281cabcf311efadf951437c9f83fa9bfe
Fernando W S Furtado <feh.wilinando@gmail.com>
2018-02-21 17:30:35 -0300

    Removendo modularização

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module br.com.caelum.mog.application {
-
-    requires spring.boot.autoconfigure;
-    requires spring.boot;
-    requires spring.beans;
-    requires spring.web;
-
-    requires br.com.caelum.mog.infra;
-}
